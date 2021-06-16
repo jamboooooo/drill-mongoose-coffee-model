d0d008b764e25f9165627fa5f78b345d45d675d4
jamboooooo <hisllam35@gmail.com>
2021-06-16 16:00:47 +0300

    commit

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -1,23 +1,23 @@
-const Drink = require('../models/Drink')
+const Drink = require("../models/Drink");
 
-const getAllDrinks = async(req, res) => {
+const getAllDrinks = async (req, res) => {
     const drinks = await Drink.find({}, { name: true, price: true });
     res.json(drinks);
-}
+};
 
-const getInStockDrinks = async(req, res) => {
+const getInStockDrinks = async (req, res) => {
     const drinks = await Drink.find({
         avail: true,
     });
     res.json(drinks);
-}
+};
 
-const getDrinkById = async(req, res) => {
+const getDrinkById = async (req, res) => {
     const drink = await Drink.findById(req.params.id);
     res.json(drink);
-}
+};
 
-const postDrink = async(req, res) => {
+const postDrink = async (req, res) => {
     try {
         const drink = await new Drink({
             name: req.body.name,
@@ -32,9 +32,9 @@ const postDrink = async(req, res) => {
     } catch (e) {
         console.log(e.message);
     }
-}
+};
 
-const deleteDrink = async(req, res) => {
+const deleteDrink = async (req, res) => {
     try {
         const drink = await Drink.findById(req.params.id);
         drink.delete();
@@ -42,16 +42,26 @@ const deleteDrink = async(req, res) => {
     } catch (e) {
         console.log(e.message);
     }
-}
+};
 
-const patchDrink = async(req, res) => {
+const patchDrink = async (req, res) => {
     try {
-        const drink = await Drink.findOneAndUpdate({ _id: req.params.id }, {...req.body })
-        await drink.save()
-        res.json('Success change');
+        const drink = await Drink.findOneAndUpdate(
+            { _id: req.params.id },
+            { ...req.body }
+        );
+        await drink.save();
+        res.json("Success change");
     } catch (e) {
         console.log(e.message);
     }
-}
+};
 
-module.exports = { getAllDrinks, getInStockDrinks, getDrinkById, postDrink, deleteDrink, patchDrink }
+module.exports = {
+    getAllDrinks,
+    getInStockDrinks,
+    getDrinkById,
+    postDrink,
+    deleteDrink,
+    patchDrink,
+};
